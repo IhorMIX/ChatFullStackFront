e3440c3bb53f4ae5ae2ae769d497aec6015c3542
Ігор Колобилін <igor.kolobulin@gmail.com>
2024-10-27 23:45:33 +0200

    joinchat logic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import WaitingRoom from './components/WaitingRoom';
+import { HubConnectionBuilder } from '@microsoft/signalr';
 
 function App() {
+  const joinChat = async (userName, chatRoom) => {
+    var connection = new HubConnectionBuilder()
+    .withUrl("http://localhost:5038/chat")
+    .withAutomaticReconnect()
+    .build();
+
+    try{
+      await connection.start()
+      await connection.invoke("JoinChat", {userName, chatRoom})
+
+      console.log(connection);  
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
+
   return (
     <ChakraProvider>
       <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-        <WaitingRoom/>
+        <WaitingRoom joinChat={joinChat}/>
       </div>
     </ChakraProvider>
   );
--- src/components/WaitingRoom.jsx
+++ src/components/WaitingRoom.jsx
@@ -1,16 +1,25 @@
 import { Button, Heading, Input, Text } from "@chakra-ui/react";
+import { useState } from "react";
 
-export const WaitingRoom = () => {
+export const WaitingRoom = ({joinChat}) => {
+  const [userName, setUserName] = useState();
+  const [chatRoom, setChatRoom] = useState();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    joinChat(userName, chatRoom);
+  }
+
   return (
-    <form className="max-w-sm w-full bg-white p-8 rounded shadow-lg">
+    <form onSubmit={onSubmit} className="max-w-sm w-full bg-white p-8 rounded shadow-lg">
       <Heading>Online</Heading>
       <div className="mb-4">
         <Text fontSize={"sm"}>User Name</Text>
-        <Input name="userName" placeholder="Input your name" />
+        <Input onChange={(e)=>setUserName(e.target.value)} name="userName" placeholder="Input your name" />
       </div>
       <div className="mb-4">
         <Text fontSize={"sm"}>Chat Name</Text>
-        <Input name="charRoom" placeholder="Input chat name" />
+        <Input onChange={(e)=>setChatRoom(e.target.value)} name="charRoom" placeholder="Input chat name" />
       </div>
       <Button type="submit" colorScheme="blue">
         Join
