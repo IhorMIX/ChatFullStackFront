641fb9774c95c0b11eb3c91cf3995e3c455ca21c
Ігор Колобилін <igor.kolobulin@gmail.com>
2024-10-28 21:40:17 +0200

    other logic + scroll

--- src/App.js
+++ src/App.js
@@ -1,33 +1,47 @@
-import React from 'react';
-import { ChakraProvider } from '@chakra-ui/react';
+import React, { useState } from 'react';
 import WaitingRoom from './components/WaitingRoom';
+import Chat from './components/Chat';
 import { HubConnectionBuilder } from '@microsoft/signalr';
 
 function App() {
+  const [connection, setConnection] = useState(null);
+  const [chatRoom, setChatRoom] = useState("");
+  const [messages, setMessages] = useState([]);
+
   const joinChat = async (userName, chatRoom) => {
     var connection = new HubConnectionBuilder()
-    .withUrl("http://localhost:5038/chat")
-    .withAutomaticReconnect()
-    .build();
+      .withUrl("http://localhost:5038/chat")
+      .withAutomaticReconnect()
+      .build();
+    
+    connection.on("ReceiveMessage", (userName, message) => {
+      setMessages((messages)=> [...messages, {userName, message}]);
+    });
 
     try{
       await connection.start()
       await connection.invoke("JoinChat", {userName, chatRoom})
 
-      console.log(connection);  
+      setConnection(connection);
+      setChatRoom(chatRoom);
     } catch(error) {
       console.log(error);
     }
   }
 
+  const sendMessage = (message) => {
+    connection.invoke("SendMessage", message)
+  }
+
+  const closeChat = async () => {
+    await connection.stop();
+    setConnection(null);
+  }
 
   return (
-    <ChakraProvider>
       <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-        <WaitingRoom joinChat={joinChat}/>
+        {connection ? <Chat messages={messages} chatRoom={chatRoom} sendMessage={sendMessage} closeChat={closeChat}/> : <WaitingRoom joinChat={joinChat}/>}
       </div>
-    </ChakraProvider>
   );
 }
-
 export default App;
